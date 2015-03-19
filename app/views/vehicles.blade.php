6eb20a722c6abf0442ca8bd318f72b676fef11b4
Tom Witkowski <dev.gummibeer@gmail.com>
2015-03-19 13:42:14 +0100

    Vehiclesuche erweitert

--- app/views/vehicles.blade.php
+++ app/views/vehicles.blade.php
@@ -10,14 +10,24 @@
 
 <h2>Fahrzeuge @if($database == 'arma3life')<span class="label label-danger">LIVE</span>@endif</h2>
 
-{{ Form::open(array('url'=>'vehicles', 'method'=>'GET')) }}
-    <div class="input-group">
-        <input type="text" name="s" class="form-control" placeholder="Spieler-ID || Klassenname" value="{{ $search }}">
-        <span class="input-group-btn">
-            <button class="btn btn-primary" type="submit">suchen</button>
-        </span>
+<div class="row">
+    <div class="col-md-10">
+        {{ Form::open(array('url'=>'vehicles', 'method'=>'GET')) }}
+        <div class="input-group">
+            <input type="text" name="s" class="form-control" placeholder="Spieler-ID || Klassenname" value="{{ $search }}">
+            <span class="input-group-btn">
+                <button class="btn btn-primary" type="submit">suchen</button>
+            </span>
+        </div>
+        {{ Form::close() }}
     </div>
-{{ Form::close() }}
+    <div class="col-md-1">
+        <a href="?t=alive" class="btn btn-primary btn-block">ganz</a>
+    </div>
+    <div class="col-md-1">
+        <a href="?t=destroyed" class="btn btn-primary btn-block">kaputt</a>
+    </div>
+</div>
 
 @if(Session::has('message') && Session::has('type'))
     <div class="alert alert-dismissible alert-{{ Session::get('type') }}">
@@ -55,7 +65,7 @@
             </td>
             <td>{{ Form::checkbox('alive', '1', $vehicle->alive); }}</td>
             <td>{{ Form::checkbox('active', '1', $vehicle->active); }}</td>
-            <td>{{ Form::checkbox('delete', '1', $vehicle->delete); }}</td>
+            <td>{{ Form::checkbox('delete', '1'); }}</td>
 
             <td>
                 <input type="hidden" name="vehicleid" value="{{ $vehicle->id }}" />
