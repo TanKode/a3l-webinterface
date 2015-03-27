ce53ca2636efe58ad800fa42b7b8f8ee9caedd2a
Tom Witkowski <dev.gummibeer@gmail.com>
2015-03-28 00:35:20 +0100

    Footer Version

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -10,7 +10,7 @@
 
 <footer>
     <p class="well">
-        Arma 3 Altis Life WebInterface v1.0 Beta |
+        Arma 3 Altis Life WebInterface v1.1 |
         developed by <a href="https://github.com/Gummibeer" target="_blank">Gummibeer</a> &copy; 2015 |
         Repository on <a href="https://bitbucket.org/Gummibeer/al-admin-tool" target="_blank">BitBucket</a> |
         licensed under <a href="http://opensource.org/licenses/MIT" target="_blank">MIT License</a>
