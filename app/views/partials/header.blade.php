7201d83723244ad7af836bc4cbe07a71cacaff27
Tom Witkowski <dev.gummibeer@gmail.com>
2015-04-01 15:25:30 +0200

    Navigationspunkte umbenannt

--- app/views/partials/header.blade.php
+++ app/views/partials/header.blade.php
@@ -35,10 +35,10 @@
             @if(Auth::user()->level >= 4)
                 <li @if(Request::is('donators'))class="active"@endif><a href="{{ url('donators') }}"><i class="icon-bill"></i> Donatoren <span class="badge">{{ $counter['donators'] }}</span></a></li>
                 <li @if(Request::is('logs'))class="active"@endif><a href="{{ url('logs') }}"><i class="icon-rawaccesslogs"></i> Logs <span class="badge">{{ $counter['logs'] }}</span></a></li>
-                <li @if(Request::is('webuser'))class="active"@endif><a href="{{ url('webuser') }}"><i class="icon-supportalt"></i> Web-User <span class="badge">{{ $counter['users'] }}</span></a></li>
+                <li @if(Request::is('webuser'))class="active"@endif><a href="{{ url('webuser') }}"><i class="icon-supportalt"></i> Webnutzer <span class="badge">{{ $counter['users'] }}</span></a></li>
             @endif
             @if(Auth::user()->level = 5)
-                <li><a href="{{ url('clearcache') }}"><i class="icon-databasedelete"></i> Seiten-Cache löschen</a></li>
+                <li><a href="{{ url('clearcache') }}"><i class="icon-databasedelete"></i> Cache löschen</a></li>
             @endif
             <li><a href="{{ url('user/logout') }}"><i class="icon-key"></i> abmelden</a></li>
         </ul>
