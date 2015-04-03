79d52ce448143ba2c41b2ae6427690d3b81b6a5b
Tom Witkowski <dev.gummibeer@gmail.com>
2015-04-03 02:08:12 +0200

    v1.5 & jQueryUI Theme edited & Begründung Dropdown max-height

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -10,7 +10,7 @@
 
 <footer>
     <p class="well">
-        Arma 3 Altis Life WebInterface v1.4 |
+        Arma 3 Altis Life WebInterface v1.5 |
         developed by <a href="https://github.com/Gummibeer" target="_blank">Gummibeer</a> &copy; 2015 |
         Repository on <a href="https://github.com/Gummibeer/a3l-webinterface" target="_blank">GitHub</a> |
         licensed under <a href="http://opensource.org/licenses/MIT" target="_blank">MIT License</a>
