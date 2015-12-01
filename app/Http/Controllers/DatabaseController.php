15699fabb2be99d353bf7685945715a2b292e000
Tom Witkowski <dev.gummibeer@gmail.com>
2015-12-01 16:40:31 +0100

    update database controller

--- app/Http/Controllers/DatabaseController.php
+++ app/Http/Controllers/DatabaseController.php
@@ -1,43 +1,30 @@
 <?php namespace A3LWebInterface\Http\Controllers;
 
-use A3LWebInterface\Http\Requests\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Redirect;
+use Illuminate\Http\Response;
 
 class DatabaseController extends Controller
 {
     public function __construct()
     {
         $this->middleware('auth', ['except' => ['backup', 'backup_database']]);
+        if (!\Auth::User()->isAllowed('view_database')) {
+            return redirect('/');
+        }
     }
 
     public function index()
     {
-        if (!\Auth::User()->isAllowed('view_database')) {
-            return Redirect::to('/');
-        }
-
         return view('database.list');
     }
 
     public function download($filename)
     {
-        if (!\Auth::User()->isAllowed('view_database')) {
-            return Redirect::to('/');
-        }
-
-        $file = \Storage::disk('local')->get('backups/' . $filename);
-        return (new \Illuminate\Http\Response($file, 200))->header('Content-Type', 'application/octet-stream');
+        return (new Response(\Storage::get('backups/' . $filename), 200))->header('Content-Type', 'application/octet-stream');
     }
 
     public function delete($filename)
     {
-        if (!\Auth::User()->isAllowed('view_database')) {
-            return Redirect::to('/');
-        }
-
-        $file = \Storage::disk('local')->delete('backups/' . $filename);
+        \Storage::delete('backups/' . $filename);
         return redirect()->route('db.list');
     }
-
 }
