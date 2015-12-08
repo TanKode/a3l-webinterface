f363404b98e3810e151c1f398218d0f68541e2ac
Tom Witkowski <dev.gummibeer@gmail.com>
2015-12-08 10:53:29 +0100

    update auth view

--- resources/views/auth.blade.php
+++ resources/views/auth.blade.php
@@ -9,6 +9,15 @@
                 <h1 class="white">{{ trans('messages.title') }}</h1>
             </div>
 
+            <div class="row">
+                <div class="col-md-6">
+                    <p><a href="{{ url('auth/login') }}" class="btn btn-block text-uppercase @if(Request::is('auth/login')) btn-default @else btn-primary @endif">{{ trans('messages.signin') }}</a></p>
+                </div>
+                <div class="col-md-6">
+                    <p><a href="{{ url('auth/register') }}" class="btn btn-block text-uppercase @if(Request::is('auth/register')) btn-default @else btn-primary @endif">{{ trans('messages.signup') }}</a></p>
+                </div>
+            </div>
+
             @yield('content')
 
             <div class="text-center">
