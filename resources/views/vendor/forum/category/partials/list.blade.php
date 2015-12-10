c0375a54fbcaa14ddb75d3f4bfeebf1725d6fe22
Tom Witkowski <dev.gummibeer@gmail.com>
2015-12-10 23:45:26 +0100

    use forum route fix

--- resources/views/vendor/forum/category/partials/list.blade.php
+++ resources/views/vendor/forum/category/partials/list.blade.php
@@ -1,6 +1,6 @@
 <tr>
     <td {{ $category->threadsEnabled ? '' : 'colspan=5'}}>
-        <a href="{{-- $category->route --}}">{{ $category->title }}</a>
+        <a href="{{ \Forum::route('category.show', $category) }}">{{ $category->title }}</a>
         @if(!empty($category->description))
             <p class="text-muted">{{ $category->description }}</p>
         @endif
@@ -9,16 +9,16 @@
         <td>{{ $category->threadCount }}</td>
         <td>{{ $category->postCount }}</td>
         <td>
-            @if ($category->newestThread)
-                <a href="{{-- $category->newestThread->route --}}">
+            @if($category->newestThread)
+                <a href="{{ \Forum::route('forum.thread.show', $category->newestThread) }}">
                     {{ $category->newestThread->title }}
                     ({{ $category->newestThread->authorName }})
                 </a>
             @endif
         </td>
         <td>
-            @if ($category->latestActiveThread)
-                <a href="{{-- $category->latestActiveThread->lastPost->url --}}">
+            @if($category->latestActiveThread)
+                <a href="{{ \Forum::route('forum.post.show', $category->latestActiveThread->lastPost) }}">
                     {{ $category->latestActiveThread->title }}
                     ({{ $category->latestActiveThread->lastPost->authorName }})
                 </a>
