08db41901edfab492676db6d264d953c5f9cecc6
Tom Witkowski <dev.gummibeer@gmail.com>
2016-01-21 18:15:10 +0100

    start trying google drive implementation

--- app/Http/Controllers/App/DashboardController.php
+++ app/Http/Controllers/App/DashboardController.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use League\Flysystem\Filesystem;
 
 class DashboardController extends Controller
 {
@@ -17,6 +18,21 @@ class DashboardController extends Controller
 
     public function getTest()
     {
+        \Config::set('app.debug', true);
+
+//        $client = new \Google_Client();
+//        $credentials = $client->loadServiceAccountJson(storage_path('app/A3L-Backup-474d4bbe7ac8.json'), 'https://www.googleapis.com/auth/drive');
+//        $client->setAssertionCredentials($credentials);
+//
+//        $service = new \Google_Service_Drive($client);
+//        dd($service->about->get());
+//        $adapter = new \Ignited\Flysystem\GoogleDrive\GoogleDriveAdapter($service);
+//
+//        $filesystem = new Filesystem($adapter);
+//
+//        dd($filesystem->read('/test/test.txt'));
+//        dd($filesystem->write('/test/test.txt', 'Hallo Welt'));
+
         abort(403);
     }
 }
