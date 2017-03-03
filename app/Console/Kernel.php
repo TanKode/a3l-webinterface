fe69148fd7033cdb0e882fe1808f98d3ca472551
Tom Witkowski <dev.gummibeer@gmail.com>
2017-03-03 20:21:52 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,12 +2,6 @@
 
 namespace App\Console;
 
-use Carbon\Carbon;
-use App\Console\Commands\LottoDraw;
-use App\Console\Commands\CreateStats;
-use App\Console\Commands\LottoCreate;
-use App\Console\Commands\CarInsurance;
-use App\Console\Commands\BackupDatabase;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
