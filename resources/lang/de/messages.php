c52fc14972940cc5b9e73a218914bf170ea4951a
Tom Witkowski <dev.gummibeer@gmail.com>
2017-03-16 10:52:54 +0100

    increase amount of cop levels

--- resources/lang/de/messages.php
+++ resources/lang/de/messages.php
@@ -128,6 +128,12 @@ return [
         5 => 'Level 5',
         6 => 'Level 6',
         7 => 'Level 7',
+        8 => 'Level 8',
+        9 => 'Level 9',
+        10 => 'Level 10',
+        11 => 'Level 11',
+        12 => 'Level 12',
+        13 => 'Level 13',
     ],
     'mediclevel'    => [
         0 => 'Zivilist',
